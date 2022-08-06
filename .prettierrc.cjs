c66886d6d4e443ad7f772fd1312ddcd56edd7187
jiaulislam <kmjiaulislam@gmail.com>
2022-08-06 14:50:31 +0600

    fix: fixed the prettier formatter & eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  max_line_length: 30,
+  bracketSpacing: true,
+  bracketSameLine: false
+};
--- chapters/chapter01/01-02.js
+++ chapters/chapter01/01-02.js
@@ -1,12 +1,28 @@
-import { AxesHelper, BoxGeometry, Color, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneGeometry, Scene, SphereGeometry, WebGLRenderer } from 'three';
+import {
+  AxesHelper,
+  BoxGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Scene,
+  SphereGeometry,
+  WebGLRenderer
+} from 'three';
 
 export function init() {
   // Scene describes a placeholer where the WebGL is rendered
   let scene = new Scene();
   // Camera describes what will camera / eye see and how will it see it
-  let camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  let camera = new PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
   // WebGLRenderer is just a initiator of browser to tell the browser that this is a WebGLRenderer Component
-  let renderer = new  WebGLRenderer();
+  let renderer = new WebGLRenderer();
 
   // set the background color of the renderer to black
   renderer.setClearColor(new Color(0x000000));
@@ -21,34 +37,34 @@ export function init() {
   // Material -> Describes how the object will look like in visual perspective accents like color, solid or not etc
   let planeGeometry = new PlaneGeometry(60, 20);
   let planeMaterial = new MeshBasicMaterial({
-    color: 0xAAAAAA
+    color: 0xaaaaaa,
   });
 
   // Create a flat plane
   let plane = new Mesh(planeGeometry, planeMaterial);
   plane.rotation.x = -0.5 * Math.PI;
-  plane.position.set(15,0,0);
+  plane.position.set(15, 0, 0);
   scene.add(plane);
 
   // create a cube
-  let cubeGeometry =  new BoxGeometry(4,4,4);
+  let cubeGeometry = new BoxGeometry(4, 4, 4);
   let cubeMaterial = new MeshBasicMaterial({
-    color: 0xFF0000,
-    wireframe: true
+    color: 0xff0000,
+    wireframe: true,
   });
 
   let cube = new Mesh(cubeGeometry, cubeMaterial);
-  cube.position.set(-4,3,0);
+  cube.position.set(-4, 3, 0);
   scene.add(cube);
   // create a sphere
-  let sphereGeometry = new SphereGeometry(4,20,20);
+  let sphereGeometry = new SphereGeometry(4, 20, 20);
   let sphereMaterial = new MeshBasicMaterial({
-    color: 0x7777FF,
-    wireframe: true
+    color: 0x7777ff,
+    wireframe: true,
   });
 
   let sphere = new Mesh(sphereGeometry, sphereMaterial);
-  sphere.position.set(20,4,2);
+  sphere.position.set(20, 4, 2);
   scene.add(sphere);
 
   // position & point the camera to the center of the scene
